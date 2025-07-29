1271b2e45631558f54de18a904ad6ca2b4e2b4ba
tvisha03 <tvishakhanna1557@gmail.com>
2025-07-29 17:48:15 +0530

    added trapping rain water problem solutions

---
+++ Stacks & Queues/Trapping-rain-water-approach2.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <stack>
+#include <iostream>
+using namespace std;
+
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
+        int left=0,right=n-1;
+        int leftMax=0,rightMax=0;
+        int total=0;
+
+        while(left<=right){
+            if(height[left]<=height[right]){
+                if(height[left]>=leftMax){
+                    leftMax=height[left];
+                }else{
+                    total+=leftMax-height[left];
+                }
+                left++;
+            }
+            else{
+                if(height[right]>=rightMax){
+                    rightMax=height[right];
+                }else{
+                    total+=rightMax-height[right];
+                }
+                right--;
+            }
+        }
+        return total;
+
+        
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> height = {0,1,0,2,1,0,1,3,2,1,2,1};
+    int result = solution.trap(height);
+    cout << "Trapped rainwater: " << result << endl;
+    return 0;
+}
