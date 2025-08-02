75f19ffdd8c55add4f67046df40c821520fb9c8b
tvisha03 <tvishakhanna1557@gmail.com>
2025-08-02 09:13:59 +0530

    added reverse-words-in-a-string.cpp

---
+++ top-interview-150/reverse-words-in-a-string.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        reverse(s.begin(),s.end());
+        int n=s.size();
+        int left=0;
+        int right=0;
+        int i=0;
+        while(i<n){
+            while(i<n && s[i]==' '){
+                i++;
+            }
+            if(i==n){
+                break;
+            }
+            while(i<n && s[i]!=' '){
+                s[right++]=s[i++];
+
+            }
+            reverse(s.begin()+left,s.begin()+right);
+            s[right++]=' ';
+            left=right;
+            i++;
+        }
+        s.resize(right-1);
+        return s;
+        
+    }
+};
+
+int main() {
+    // Example usage:
+    Solution solution;
+    string s = "the sky is blue"; // Example input
+    string reversed = solution.reverseWords(s);
+    
+    // Output the reversed words
+    cout << "Reversed Words: " << reversed << endl; // Output: "blue is sky the"
+    
+    return 0;
+}
